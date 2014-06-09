56c0a1cdfc0a46d2bdd1bc6a9587a6b8efbd419f
uygned <uygned@gmail.com>
2014-06-09 19:02:16 +0800

    -bugfix- no contour overwrite on slimmed point

--- src/font_test.c
+++ src/font_test.c
@@ -4,29 +4,26 @@
 #include <locale.h>
 #include <stdint.h>
 
-wchar_t *text = L"meyLinux"; // abdfghpqy// Windows Steve 中文字体ɒɔ
+wchar_t *text = L"Black"; // meyLinux // abdfghpqy// Windows Steve 中文字体ɒɔ
 
-int font_height = 14;
+int font_height = 13;
 int grid_size = 16; // in pixel
-#define RASTER_GLYPH_GRIDS
-//#define RASTER_GLYPH_POINTS
-int fill_contour = 1;
+#define DRAW_GRIDLINES
+//#define DRAW_OUTLINEPOINTS
+//#define FILL_CONTOURS 0
+#define FILL_CONTOURS 1
+//#define BLACK_ON_WHITE 0
+#define BLACK_ON_WHITE 1 /* or WHITE_ON_BLACK */
+
 FT_ULong load_flags = FT_LOAD_NO_HINTING;
 //FT_ULong load_flags = FT_LOAD_NO_AUTOHINT;
 //FT_ULong load_flags = FT_LOAD_FORCE_AUTOHINT;
-int black_on_white = 1;
 
-//color_t color_r = { 0xff, 0x00, 0x00 };
-//color_t color_g = { 0x00, 0xff, 0x00 };
-//color_t color_b = { 0x00, 0x00, 0xff };
-//color_t color_z = { 0x00, 0x00, 0x00 };
-//color_t *contour_color = &color_r;
-//int clean_c = 0;
-//int contour_c = 1;
-//int filling_c = 2;
+int subpixel_rendering = 1;
 
 /* color component-value pair */
-unsigned char contour_colors[3] = { 1, 2, 3 };
+unsigned char contour_colors[3] = { 1, 2 };
+unsigned char slim_color = 4;
 unsigned char filling_color = 0xff;
 unsigned char debug_color = 0x80;
 unsigned char grid_color = 0x00;
@@ -87,6 +84,16 @@ void bitmap_shift(bitmap_t *bmp, int shift_x, int shift_y) {
 	bmp->data += shift_y * bmp->bytes_per_line + shift_x * bmp->bytes_per_pixel;
 }
 
+inline void raster_set_contour(unsigned char *d, unsigned char color) {
+	if (d[0] != slim_color)
+		d[0] = color;
+}
+
+inline void raster_set_pixel(bitmap_t *r, int x, int y, unsigned char color) {
+	unsigned char *d = r->data + y * r->bytes_per_line + x * r->bytes_per_pixel;
+	d[0] = color;
+}
+
 inline int raster_on_contour(unsigned char *s) {
 	return s[0] == contour_colors[0] || s[0] == contour_colors[1];
 }
@@ -107,7 +114,7 @@ inline void raster_copy_contour(bitmap_t *dst, bitmap_t *src, int width,
 				d[2] = 0xff, d[1] = 0x00, d[0] = 0x00;
 			else if (s[0] == contour_colors[1])
 				d[2] = 0x00, d[1] = 0xff, d[0] = 0x00;
-			else if (s[0] == contour_colors[2])
+			else if (s[0] == slim_color)
 				d[2] = 0x00, d[1] = 0x00, d[0] = 0xff;
 			else if (s[0] == filling_color)
 				d[2] = 0x00, d[1] = 0x00, d[0] = 0x00;
@@ -155,7 +162,7 @@ inline int raster_slim_contour(bitmap_t *canvas, FT_Vector *p0) {
 			 *        xx*    *x*
 			 */
 			if (bl || br) {
-				s[0] = contour_colors[2];
+				s[0] = slim_color;
 				FT_Vector p = *p0;
 				p.y++;
 				raster_slim_contour(canvas, &p);
@@ -172,7 +179,7 @@ inline int raster_slim_contour(bitmap_t *canvas, FT_Vector *p0) {
 			 * below: -x-    -x-
 			 */
 			else if (!(l || r)) {
-				s[0] = contour_colors[2];
+				s[0] = slim_color;
 				FT_Vector p = *p0;
 				p.y++;
 				raster_slim_contour(canvas, &p);
@@ -189,7 +196,7 @@ inline int raster_slim_contour(bitmap_t *canvas, FT_Vector *p0) {
 		if (a) {
 			slim = 1;
 			if (al || ar) {
-				s[0] = contour_colors[2];
+				s[0] = slim_color;
 				FT_Vector p = *p0;
 				p.y--;
 				raster_slim_contour(canvas, &p);
@@ -201,7 +208,7 @@ inline int raster_slim_contour(bitmap_t *canvas, FT_Vector *p0) {
 					raster_slim_contour(canvas, &p);
 				}
 			} else if (!(l || r)) {
-				s[0] = contour_colors[2];
+				s[0] = slim_color;
 				FT_Vector p = *p0;
 				p.y--;
 				raster_slim_contour(canvas, &p);
@@ -231,7 +238,7 @@ inline int raster_slim_contour(bitmap_t *canvas, FT_Vector *p0) {
 #endif
 
 	if (slim) {
-		s[0] = contour_colors[2];
+		s[0] = slim_color;
 		return 1;
 	}
 	return 0;
@@ -284,21 +291,47 @@ void raster_fill_contour(bitmap_t *dst, bitmap_t *src, int x, int y, int dst_w,
 	int src_line_incr = src_line_size * grid_size;
 	int src_grid_incr = src_pixel_size * grid_size;
 
-	const int grid_weight = grid_size * grid_size;
+	const unsigned grid_weight = grid_size * grid_size;
 	double coverage;
+	// subpixel related variables
+	double *coverages = NULL;
+	unsigned *subpixel_sizes = NULL;
+	unsigned *subpixel_weights = NULL;
+	unsigned subpixel_width = dst_w * 3 + 4;
+	unsigned char *subpixel_data = NULL;
+	if (subpixel_rendering) {
+		coverages = malloc(sizeof(double) * 3);
+		subpixel_sizes = malloc(sizeof(unsigned) * 3);
+		subpixel_weights = malloc(sizeof(int) * 3);
+		subpixel_data = malloc(subpixel_width);
+		subpixel_sizes[0] = grid_size / 3;
+		subpixel_sizes[1] = (grid_size - subpixel_sizes[0]) / 2;
+		subpixel_sizes[2] = grid_size - subpixel_sizes[0] - subpixel_sizes[1];
+		subpixel_weights[0] = subpixel_sizes[0] * grid_size;
+		subpixel_weights[1] = subpixel_sizes[1] * grid_size;
+		subpixel_weights[2] = subpixel_sizes[2] * grid_size;
+		printf("rgb size: %d,%d,%d\n", subpixel_sizes[0], subpixel_sizes[2],
+				subpixel_sizes[2]);
+	}
+
 	char *prev_on = malloc(grid_size);
 	char *fill_flags = malloc(grid_size);
-	int i, j;
+	int i, j, k, l, si;
 	for (j = 0; j < dst_h; ++j, (dst_line += dst_line_size, src_line_outer +=
 			src_line_incr)) {
 		d = dst_line;
 		s_outer = src_line_outer;
 		memset(prev_on, 0, grid_size);
 		memset(fill_flags, 0, grid_size);
-		for (i = 0; i < dst_w; i++, (d += dst_pixel_size, s_outer +=
+		if (subpixel_rendering)
+			memset(subpixel_data, 0, subpixel_width);
+		for (i = 0; i < dst_w; ++i, (d += dst_pixel_size, s_outer +=
 				src_grid_incr)) {
 			// calculate coverage of grid (i, j)
-			coverage = 0;
+			if (subpixel_rendering)
+				memset(coverages, 0, sizeof(double) * 3);
+			else
+				coverage = 0;
 			src_line = s_outer;
 			for (y = 0; y < grid_size; y++, (src_line += src_line_size)) {
 				s = src_line;
@@ -338,22 +371,63 @@ void raster_fill_contour(bitmap_t *dst, bitmap_t *src, int x, int y, int dst_w,
 							s[0] = filling_color;
 					}
 
-					if (s[0] != 0)
-						coverage++;
+					if (s[0] > 0) {
+						if (subpixel_rendering) {
+							if (x >= subpixel_sizes[0] + subpixel_sizes[1])
+								coverages[2]++;
+							else if (x >= subpixel_sizes[0])
+								coverages[1]++;
+							else
+								coverages[0]++;
+						} else {
+							coverage++;
+						}
+					}
 				}
 			}
-			if (coverage > 0) {
+			if (subpixel_rendering) {
+				for (k = 0; k < 3; k++) {
+					if (coverages[k] == 0)
+						continue;
+					coverage = coverages[k] / subpixel_weights[k];
+					if (coverage > 1)
+						perror("coverage > 1");
+//					printf("%f %.0f/%d\n", coverage, coverages[k], subpixel_weights[k]);
+					si = i * 3 + k; // subpixel index
+					for (l = 0; l < 5; l++)
+						subpixel_data[si + l] += coverage * subpixel_filter[l];
+				}
+//				d[0] = 0xff * coverages[2], d[1] = 0xff * coverages[1], d[2] =
+//						0xff * coverages[0];
+			} else if (coverage > 0) {
 				coverage = coverage / grid_weight;
-				if (black_on_white)
+				if (BLACK_ON_WHITE)
 					coverage = 1 - coverage;
 				d[0] = 0xff * coverage, d[1] = 0xff * coverage, d[2] = 0xff
 						* coverage;
-				printf("%d %d = %.0f/%.0f\n", x, y, coverage, grid_weight);
+//				printf("%d %d = %.0f/%.0f\n", x, y, coverage, grid_weight);
+			}
+
+		}
+		if (subpixel_rendering) {
+			d = dst_line;
+			for (i = 2; i < subpixel_width - 2;) {
+				d[2] = 0xff - subpixel_data[i++];
+				d[1] = 0xff - subpixel_data[i++];
+				d[0] = 0xff - subpixel_data[i++];
+				d += dst_pixel_size;
 			}
 		}
 	}
+
 	free(prev_on);
 	free(fill_flags);
+	if (coverages)
+		free(coverages);
+	if (subpixel_sizes)
+		free(subpixel_sizes);
+	if (subpixel_data)
+		free(subpixel_data);
 }
 
 int raster_draw_outline(bitmap_t *canvas, wchar_t c, font_conf_t *font,
@@ -419,7 +493,7 @@ int raster_draw_outline(bitmap_t *canvas, wchar_t c, font_conf_t *font,
 
 	int contour_draws = 0;
 	char tag_mode = FT_CURVE_TAG_ON | FT_CURVE_TAG_CONIC | FT_CURVE_TAG_CUBIC;
-	char *tags[] = { "conic", "on", "cubic" };
+	char *tags[] = { "CONIC", "ON", "CUBIC" };
 	FT_Vector *contour_start = NULL;
 	int is_contour_end = 0;
 	for (i = 0; i < outline->n_points; i++) {
@@ -432,8 +506,8 @@ int raster_draw_outline(bitmap_t *canvas, wchar_t c, font_conf_t *font,
 
 		// label the control point
 		buf[0] = i % 2 == 0 ? (char) ('A' + i / 2) : (char) ('a' + i / 2);
-		printf("%d.%s/%d %ld,%ld %c %d\n", j, tags[tag & tag_mode], tag,
-				curr->x, curr->y, buf[0], tag > 2 ? tag : 0);
+		printf("C%d %s(%d) %03ld,%03ld %c", j, tags[tag & tag_mode], tag,
+				curr->x, curr->y, buf[0]);
 
 		if (contour_start == NULL)
 			contour_start = curr;
@@ -446,7 +520,7 @@ int raster_draw_outline(bitmap_t *canvas, wchar_t c, font_conf_t *font,
 		// http://freetype.org/freetype2/docs/reference/ft2-outline_processing.html#FT_Outline
 		if (tag & FT_CURVE_TAG_ON) {
 			if (last_on) {
-				printf(" |%s\n", last_conic ? "conic" : "line");
+				printf(" |%s", last_conic ? "conic" : "line");
 				if (last_conic) {
 					draw_conic(canvas, last_on, last_conic, curr,
 							contour_colors[contour_draws++ % 2]);
@@ -489,7 +563,7 @@ int raster_draw_outline(bitmap_t *canvas, wchar_t c, font_conf_t *font,
 							contour_colors[contour_draws++ % 2]);
 					if (contour_draws > 1)
 						raster_slim_contour(canvas, &temp);
-					printf(" |on/conic %ld %ld - %ld %ld\n", temp.x, temp.y,
+					printf(" |on/conic %ld %ld - %ld %ld", temp.x, temp.y,
 							virtual_on.x, virtual_on.y);
 				}
 				last_on = &virtual_on;
@@ -501,7 +575,7 @@ int raster_draw_outline(bitmap_t *canvas, wchar_t c, font_conf_t *font,
 					if (contour_draws > 1)
 						raster_slim_contour(canvas, last_on);
 				}
-				printf(" |on/conic %ld %ld - %ld %ld\n", last_on->x, last_on->y,
+				printf(" |on/conic %ld %ld - %ld %ld", last_on->x, last_on->y,
 						contour_start->x, contour_start->y);
 				last_on = NULL;
 				last_conic = NULL;
@@ -515,10 +589,13 @@ int raster_draw_outline(bitmap_t *canvas, wchar_t c, font_conf_t *font,
 			j++;
 			contour_start = NULL;
 			contour_draws = 0;
+			printf(" *\n");
+		} else {
+			printf("\n");
 		}
 
-#ifdef RASTER_GLYPH_POINTS
-		rect_init(&rect, curr->x + 2, curr->y + 2, font->x_ppem_26_6 >> 6,
+#ifdef DRAW_OUTLINEPOINTS
+		rect_init(&rect, curr->x, curr->y, font->x_ppem_26_6 >> 6,
 				font->ascender + font->descender);
 		glyph_run_init(&grun, 0, 0);
 		wchar_t *label = mbs2wcs(buf);
@@ -527,8 +604,8 @@ int raster_draw_outline(bitmap_t *canvas, wchar_t c, font_conf_t *font,
 #endif
 	}
 
-	printf("#point=%d #contour=%d max_x=%d\n", outline->n_points,
-			outline->n_contours, max_x);
+	printf("`%lc' #contour=%d #point=%d max_x=%d\n", c, outline->n_contours,
+			outline->n_points, max_x);
 
 	int advance_x = glyph->advance.x >> 10;
 //	if (advance_x % 64 != 0)
@@ -578,8 +655,8 @@ int x11_processor(XEvent *event) {
 			font.scaler = scaler;
 		} else {
 			double grid_scale = grid_size / 64.0;
-			int offset_x = 100;
-			int offset_y = 2;
+			int offset_x = 20;
+			int offset_y = 20;
 			int origin_y = offset_y + font.ascender * grid_size;
 
 			int i, advance_x = 0;
@@ -587,11 +664,12 @@ int x11_processor(XEvent *event) {
 				advance_x += raster_draw_outline(bmp_raster, text[i], &font,
 						load_flags, offset_x + advance_x, origin_y, grid_scale);
 			}
+//			raster_set_pixel(bmp_raster, 60, 172, debug_color);
 
-			if (fill_contour) {
+			if (FILL_CONTOURS) {
 //				bitmap_t sub = *bmp_raster;
 				bitmap_t sub = bmp_canvas;
-				bitmap_shift(&sub, offset_x + advance_x + 20, offset_y + 100);
+				bitmap_shift(&sub, offset_x + advance_x + 20, offset_y);
 				raster_fill_contour(&sub, bmp_raster, offset_x, offset_y,
 						advance_x / grid_size, font.ascender + font.descender,
 						grid_size);
@@ -601,7 +679,7 @@ int x11_processor(XEvent *event) {
 			int max_y = min(origin_y + font.descender * grid_size,
 					bmp_canvas.rect.height);
 
-#ifdef RASTER_GLYPH_GRIDS
+#ifdef DRAW_GRIDLINES
 
 			printf("bbox (%d,%d) (%d,%d) descender %d\n", offset_x, offset_y,
 					max_x, max_y, font.descender);
@@ -649,7 +727,9 @@ int main(int argc, char *argv[]) {
 //	IPA fonts: CharisSIL-R.ttf DoulosSIL-R.ttf LinLibertine_R.otf
 	freetype_init(2, 2, 10240 * 1024, 1);
 //	freetype_font("fonts/XinGothic.otf", "fonts/方正兰亭细黑_GBK.ttf");
-	freetype_font("/home/pxlogpx/.fonts/_H_Menlo.ttc", 1, "fonts/arial.ttf", 0);
+	freetype_font("/home/pxlogpx/.fonts/Lucida Grande.ttf", 0,
+			"fonts/arial.ttf", 0);
+//	freetype_font("/home/pxlogpx/.fonts/_H_Menlo.ttc", 1, "fonts/arial.ttf", 0);
 //	freetype_font("/home/pxlogpx/.fonts/_H_HelveticaNeue.ttc", 4,
 //			"fonts/arial.ttf", 0);
 
